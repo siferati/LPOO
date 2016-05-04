c7aacb65a9cb24a8ba3b819a6081d0f35f035362
Tirafesi <tirafesi@hotmail.com>
2016-05-04 15:24:41 +0100

    added GameLoopActivity and it's layout

---
+++ app/src/main/java/com/example/tiago/lpoo/Layouts/GameLoopActivityLayout.java
@@ -0,0 +1,25 @@
+package com.example.tiago.lpoo.Layouts;
+
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameLoopActivityLayout extends SurfaceView implements Runnable {
+
+    public GameLoopActivityLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void resume() {
+
+    }
+}
