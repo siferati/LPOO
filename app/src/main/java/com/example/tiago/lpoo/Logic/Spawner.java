7c0a03e50e5a02f097efa0c66fdd98cb40a4bde6
RuiCS <rui.pedro.csoares@gmail.com>
2016-05-11 17:22:18 +0100

    Added spawned to spawner

--- app/src/main/java/com/example/tiago/lpoo/Logic/Spawner.java
+++ app/src/main/java/com/example/tiago/lpoo/Logic/Spawner.java
@@ -2,6 +2,8 @@ package com.example.tiago.lpoo.Logic;
 
 import android.graphics.Canvas;
 
+import java.util.ArrayList;
+
 public class Spawner {
 
     // Attributes
@@ -12,10 +14,16 @@ public class Spawner {
     private Monster prototype;
 
     /*
+     * Holds the spawned monsters
+     */
+    private ArrayList<Monster> spawned;
+
+    /*
      * Constructor
      */
     public Spawner(Monster prototype){
         this.prototype = prototype;
+        this.spawned = new ArrayList<Monster>();
     }
 
     /*
@@ -26,10 +34,34 @@ public class Spawner {
     public Monster getPrototype(){ return prototype; }
 
     /*
+     * Getter
+     *
+     * @return the spawned monsters.
+     */
+    public ArrayList<Monster> getSpawned(){ return spawned; }
+
+    /*
+     * Setter
+     * @param the spawned monsters.
+     */
+    public void setSpawned( ArrayList<Monster> spawned) { this.spawned = spawned; }
+
+    /*
+     * Setter
+     *
+     * @param the new monster
+     */
+    public void setPrototype(Monster prototype){
+        this.prototype = prototype;
+    }
+
+    /*
      * Method - Spawn a monster
      */
     public Monster spawnMonster(){
-        return prototype.clone();
+        Monster m = prototype.clone();
+        spawned.add(m);
+        return m;
     }
 
 }
