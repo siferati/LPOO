acc0f1da2b782a0029c8b4dc6b2f93fbaa5d52a5
tirafesi <tirafesi@hotmail.com>
2016-05-13 19:05:03 +0100

    Continued Sprite class

--- app/src/main/java/com/example/tiago/lpoo/Logic/Sprite.java
+++ app/src/main/java/com/example/tiago/lpoo/Logic/Sprite.java
@@ -1,7 +1,6 @@
 package com.example.tiago.lpoo.Logic;
 
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Rect;
 
 /**
@@ -14,23 +13,85 @@ public class Sprite {
     /**
      * The sprite sheet
      */
-    public Bitmap spriteSheet;
+    private Bitmap spriteSheet;
 
     /**
      * Region of the sprite sheet to draw (single sprite)
      */
-    public Rect rect;
+    private Rect src;
+
+    /**
+     * Sprite sheet's height
+     */
+    private int height;
+
+    /**
+     * Sprite sheet's width
+     */
+    private int width;
+
+    /**
+     * Number of rows in the sprite sheet
+     */
+    private int rows;
+
+    /**
+     * Number of columns in the sprite sheet
+     */
+    private int columns;
+
+    /**
+     * Height of a single sprite of the sprite sheet
+     */
+    private int spriteHeight;
+
+    /**
+     * Width of a single sprite of the sprite sheet
+     */
+    private int spriteWidth;
+
+    /**
+     * How many frames passed since this Sprite was created
+     */
+    private int frameCount;
 
     //Methods:
 
     /**
      * Constructor
      *
-     * @param spriteSheet Sprite Sheet
-     * @param rect        Region of the sprite sheet to draw (single sprite)
+     * @param spriteSheet The sprite sheet
+     * @param rows        Number of rows in the sprite sheet
+     * @param columns     Number of columns in the sprite sheet
      */
-    public Sprite(Bitmap spriteSheet, Rect rect) {
+    public Sprite(Bitmap spriteSheet, int rows, int columns) {
         this.spriteSheet = spriteSheet;
-        this.rect = rect;
+        height = spriteSheet.getHeight();
+        width = spriteSheet.getWidth();
+        this.rows = rows;
+        this.columns = columns;
+        spriteHeight = height / rows;
+        spriteWidth = width / columns;
+        //set the source rectangle (1st sprite of the sprite sheet)
+        src = new Rect(0, 0, spriteWidth, spriteHeight);
+        frameCount = 0;
+    }
+
+
+    public void update() {
+        frameCount++;
+    }
+
+    public void nextSprite() {
+        //if src.bottom yada yada
+
+        //if it's the last sprite of a row
+        if (src.right == width - 1) {
+            int previousTop = src.top;
+            //choose the 1st sprite of the next row
+            src.offsetTo(0, previousTop + spriteHeight);
+        } else
+            src.offset(spriteWidth, 0);
+
     }
 }
